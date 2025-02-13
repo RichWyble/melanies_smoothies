3bc2294fcfccd555a19372d8503386840526a07e
wybler <rich.wyble@rockymountconsulting.com>
2025-02-13 07:52:36 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,8 +42,8 @@ my_dataframe = session.execute("SELECT FRUIT_NAME, SEARCH_ON FROM smoothies.publ
 
 #Convert the my_dataframe to a pandas dataframe.
 results = my_dataframe.fetchall()
-pd_df=pd.dataframe(results, columns=["FRUIT_NAME","SEARCH_ON"])
-st.dataframe(pd_df)
+pd_df=pd.Dataframe(results, columns=["FRUIT_NAME","SEARCH_ON"])
+st.dataframe(data=pd_df, use_container_width=True)
 st.stop()
 
 ingredients_list = st.multiselect(
