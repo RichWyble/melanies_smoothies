11a8e4f616199672b7266a219f90a0bea68aa3f1
wybler <rich.wyble@rockymountconsulting.com>
2025-03-06 20:21:21 -0600

    Update streamlit_app.py
    debugging

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ if ingredients_list:
         
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
-        smoothiefroot_response.json()
+        st. write(smoothiefroot_response.json())
         st.stop()
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
