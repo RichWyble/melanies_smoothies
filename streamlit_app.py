f88486a876dab40d65503bad1a479b4101ddedad
wybler <rich.wyble@rockymountconsulting.com>
2025-03-06 18:00:51 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,83 +1,41 @@
 # Import python packages
 import streamlit as st
+from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-import snowflake.connector
-import requests
-import pandas as pd
- 
 
 # Write directly to the app
-st.title("Customize Your Smoothie :cup_with_straw:")
+st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
-    """Choose the fruits you want in your custom Smoothie!""")
+    """Choose the fruits you want in your custom Smoothie!
+    """)
 
-name_on_order = st.text_input("Name on Smoothie:")
-st.write("The name on your Smoothie will be:", name_on_order)
 
-##session = get_active_session()
-#cnx = st.connection("snowflake", 
-#    connection_args={
-#        "insecure_mode": True  # TEMPORARILY disable SSL certificate check
-#    }
-#)
-#session = cnx.session()
-# Use Streamlit secrets for credentials
-conn = snowflake.connector.connect(
-    user=st.secrets["snowflake"]["user"],
-    password=st.secrets["snowflake"]["password"],
-    account=st.secrets["snowflake"]["account"],
-    warehouse=st.secrets["snowflake"]["warehouse"],
-    database=st.secrets["snowflake"]["database"],
-    schema=st.secrets["snowflake"]["schema"],
-    role=st.secrets["snowflake"].get("role", "PUBLIC")  # Default to PUBLIC role
-)
 
-# Create a session
-session = conn.cursor()
-#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# replaced the above with what's below as part of the move to SniS
-my_dataframe = session.execute("SELECT FRUIT_NAME, SEARCH_ON FROM smoothies.public.fruit_options")
+
+session = get_active_session()
+my_dataframe = session.table('smoothies.public.fruit_options').select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
-#st.stop()
-
-#Convert the my_dataframe to a pandas dataframe.
-results = my_dataframe.fetchall()
-pd_df=pd.DataFrame(results, columns=["FRUIT_NAME","SEARCH_ON"])
-#st.dataframe(data=pd_df, use_container_width=True)
-#st.stop()
 
 ingredients_list = st.multiselect(
-    'Choose up to 5 ingredents'
+    'Choose up to 5 ingredients:'
     , my_dataframe
-    , max_selections=5
 )
 
 if ingredients_list:
 
     ingredients_string = ''
-
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-            values ('""" + ingredients_string + """', '""" + name_on_order +"""')"""
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
+            values ('""" + ingredients_string + """')"""
 
     #st.write(my_insert_stmt)
-    #st.stop()
-    
     time_to_insert = st.button('Submit Order')
-    
+
     if time_to_insert:
-        #Changes due to SniS migration.  It's a difference between using snowflake.connector (SniS) vs. Snowpark (SiS)
-        #session.sql(my_insert_stmt).collect()
-        session.execute(my_insert_stmt)
+        session.sql(my_insert_stmt).collect()
 
         st.success('Your Smoothie is ordered!', icon="✅")
